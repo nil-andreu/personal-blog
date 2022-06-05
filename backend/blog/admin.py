a77cff126c6f228145366d954b40101380085a3d
Nil ANDREU <nil.andreu@veepee.com>
2022-06-05 13:37:24 +0200

    Basic blog working

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 class PostAdmin(admin.ModelAdmin):
-
     model = Post
 
-    # Do not include the slug
+    # Do not include the slug when creating a new on the admin
     fields = (
-        'title', 'category', 'tags', 'published',
+        'title', 'category', 'tags','status',
     )
+    
+    list_display = ('title', 'category', 'tags', 'publish','status',)
+    list_filter = ('status',)
+    search_fields = ('title', 'body',)
+    
+    
 
 # Register your models here.
 admin.site.register(Post,PostAdmin)
 admin.site.register(Category)
+admin.site.register(Tag)
