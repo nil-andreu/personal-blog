fcbfee5c7295c4d09e31e40273f9ac271e76c36d
Nil ANDREU <nil.andreu@veepee.com>
2022-07-01 18:48:55 +0200

    Switched to frontend React.ts

--- cache/index.js
+++ cache/index.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import Model from "../model";
+import Model from "./model";
 
 const FrontPage = () => {
   return (
