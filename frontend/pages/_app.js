3bbb8f411a12f83379e8dc500af33e16432a7c6e
Nil ANDREU <nil.andreu@veepee.com>
2022-07-01 18:46:19 +0200

    Changing the frontend of nextjs

---
+++ frontend/pages/_app.js
@@ -0,0 +1,16 @@
+// import '../styles/globals.css'
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
+
---
+++ frontend/pages/index.js
@@ -0,0 +1,26 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Model from "../model";
+
+const FrontPage = () => {
+  return (
+    <Canvas
+      camera={{ position: [0, 0, 12], fov: 20 }}
+      style={{
+        backgroundColor: "#111a21",
+        width: "100vw",
+        height: "100vh"
+      }}
+    >
+      <ambientLight intensity={1.25} />
+      <ambientLight intensity={0.1} />
+      <directionalLight intensity={0.4} />
+
+      <Suspense fallback={null}>
+        <Model position={[0, -1, 1]} />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default FrontPage;
