d517666fa667a563f5a04752bd03034941f1e594
Nil ANDREU <nil.andreu@veepee.com>
2022-07-01 19:59:08 +0200

    Cleaning code

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -2,23 +2,23 @@ from django.contrib import admin
 from .models import Post, Category, Tag, Visit
 
 class PostAdmin(admin.ModelAdmin):
-    model = Post
+    model=Post
 
     # Do not include the slug when creating a new on the admin
-    fields = (
+    fields=(
         'title', 'body', 'category', 'tags','status',
     )
     
-    list_display = ('title','category', 'publish','status',)
-    list_filter = ('status',)
-    search_fields = ('title', 'body',)
+    list_display=('title','category', 'publish','status',)
+    list_filter=('status',)
+    search_fields=('title', 'body',)
 
 class VisitAdmin(admin.ModelAdmin):
-    model = Visit
+    model=Visit
     
-    list_display = ('post','date',)
-    list_filter = ('post','date',)
-    search_fields = ('post__title','date',)
+    list_display=('post','date',)
+    list_filter=('post','date',)
+    search_fields=('post__title','date',)
 
 # Register your models here.
 admin.site.register(Post,PostAdmin)
