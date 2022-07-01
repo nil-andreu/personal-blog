d407a624c0a2d1316926265a8640175f1e768077
Nil ANDREU <nil.andreu@veepee.com>
2022-07-01 20:38:52 +0200

    Cleaning code

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -5,8 +5,10 @@ from django.utils.text import slugify
 class Category(models.Model):
     category = models.CharField(max_length=200, unique=True,
                                 primary_key=True)
+    
     def __str__(self):
-        return self.category    
+        return self.category
+    
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
@@ -14,9 +16,11 @@ class Category(models.Model):
 
 class Tag(models.Model):
     tag = models.CharField(max_length=100, unique=True,
-                           primary_key=True)
+                            primary_key=True)
+    
     def __str__(self):
         return self.tag
+    
     class Meta:
         verbose_name = 'Tag'
         verbose_name_plural = 'Tags'
@@ -37,14 +41,17 @@ class Post(models.Model):
     publish = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=True)
+    
     def save(self, *args, **kwargs):
         # We create the slug based on the title of the post
         self.slug = slugify(self.title)  # lower-case-guions
         super(Post, self).save(*args, **kwargs)
+        
     class Meta:
         ordering = ('publish', 'title', 'status', )
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
+        
     def __str__(self):
         return self.title
 
@@ -56,8 +63,8 @@ class Visit(models.Model):
 
 
 class Comemnts(models.Model):
-    comment=models.CharField(max_length=400, unique=False, 
-                               blank=False, null=False)
-    user=models.CharField(max_length=100)
-    email=models.EmailField(blank=True, null=True)
-    post=models.ForeignKey(Post, on_delete=models.CASCADE)  # When post deleted, delete all the other things
+    comment = models.CharField(max_length=400, unique=False, 
+                                blank=False, null=False)
+    user = models.CharField(max_length=100)
+    email = models.EmailField(blank=True, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)  # When post deleted, delete all the other things
