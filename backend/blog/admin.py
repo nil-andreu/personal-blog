725ccb3f74af40a948ec0305a2a4e6a31caf7ce1
Nil ANDREU <nil.andreu@veepee.com>
2022-07-01 20:12:23 +0200

    Clearning code

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,27 +1,30 @@
 from django.contrib import admin
 from .models import Post, Category, Tag, Visit
 
+
 class PostAdmin(admin.ModelAdmin):
     model=Post
 
     # Do not include the slug when creating a new on the admin
     fields=(
-        'title', 'body', 'category', 'tags','status',
+        'title', 'body', 'category', 'tags','status', 
     )
     
-    list_display=('title','category', 'publish','status',)
-    list_filter=('status',)
-    search_fields=('title', 'body',)
+    list_display=('title','category', 'publish','status', )
+    list_filter=('status', )
+    search_fields=('title', 'body', )
+
 
 class VisitAdmin(admin.ModelAdmin):
     model=Visit
     
-    list_display=('post','date',)
-    list_filter=('post','date',)
-    search_fields=('post__title','date',)
+    list_display=('post','date', )
+    list_filter=('post','date', )
+    search_fields=('post__title','date', )
+
 
 # Register your models here.
-admin.site.register(Post,PostAdmin)
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Visit, VisitAdmin)
+admin.site.register(Post,PostAdmin, )
+admin.site.register(Category, )
+admin.site.register(Tag, )
+admin.site.register(Visit, VisitAdmin, )
