f4b78a0141377cc02188423d10d727ec2065369b
Nil ANDREU <nil.andreu@veepee.com>
2022-07-01 19:48:39 +0200

    Cleaning code

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -3,7 +3,7 @@ from django.utils.text import slugify
 
 class Category(models.Model):
     category = models.CharField(max_length = 200, unique = True, 
-        primary_key=True)
+                                primary_key=True)
     
     def __str__(self):
         return self.category
@@ -14,7 +14,7 @@ class Category(models.Model):
         
 class Tag(models.Model):
     tag = models.CharField(max_length = 100, unique = True, 
-        primary_key = True)
+                           primary_key = True)
     
     def __str__(self):
         return self.tag
@@ -27,14 +27,14 @@ class Tag(models.Model):
 class Post(models.Model):
     # Title section
     title = models.CharField(blank = False, null = False, unique = True, 
-        max_length = 200)
+                             max_length = 200)
     
     slug = models.SlugField(unique = True)
     body = models.TextField()
     
     # Define category and subcategory
     category = models.ForeignKey(Category, on_delete = models.CASCADE, 
-        blank = True, null = True)
+                                 blank = True, null = True)
     tags = models.ManyToManyField(Tag)
     
     # Important dates of the blog
@@ -62,7 +62,7 @@ class Visit(models.Model):
 
 class Comemnts(models.Model):
     comment = models.CharField(max_length = 400, unique=False, 
-        blank = False, null = False)
+                               blank = False, null = False)
     user = models.CharField(max_length = 100)
     email = models.EmailField(blank = True, null = True)
     post = models.ForeignKey(Post, on_delete = models.CASCADE) # When post deleted, delete all the other things
