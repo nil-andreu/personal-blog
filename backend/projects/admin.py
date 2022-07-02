1b41b0037e14dad1bcc6b7ab3d73805baab60830
Nil ANDREU <nil.andreu@veepee.com>
2022-07-02 14:21:21 +0200

    Resources model

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Technology, Project, Difficulties, Resource
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Difficulties)
+admin.site.register(Resource)
--- backend/projects/models.py
+++ backend/projects/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.db import models
 from django.utils.text import slugify
 from django.core.validators import MaxValueValidator, MinValueValidator
-
+from .utils.file_utils import validate_file_extension, upload_file 
 
 class Technology(models.Model):
     technology = models.CharField(max_length=200, unique=True, primary_key=True)
@@ -13,7 +13,7 @@ class Technology(models.Model):
 class Project(models.Model):
     title: string = models.CharField(max_length=300, blank=False, null=False, unique=True)
     summary: string = models.TextField(blank=True, null=True)
-    technology = models.ManyToManyField(Technology, blank=True, null=True)
+    technology = models.ManyToManyField(Technology)
     slug: string = models.SlugField()
     github: string = models.URLField()
     publish: datetime = models.DateTimeField(auto_now_add=True)
@@ -31,3 +31,20 @@ class Project(models.Model):
     def save(self, *args, **kwargs):
         self.slug: string = slugify(self.title)  # lower-case-guions
         super(Project, self).save(*args, **kwargs)
+
+
+class Difficulties(models.Model):
+    title = models.CharField(max_length=300)  # expressing difficulties of the project in a text, and how were overcame
+    description = models.TextField()
+    solution = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
+
+
+class Resource(models.Model):
+    title = models.CharField(max_length=300, blank=True, null=True, unique=True)
+    description = models.TextField(blank=True, null=True)
+    
+    file = models.FileField(upload_to=upload_file, validators=[validate_file_extension])
+
+    def __str__(self):
+        return self.title
