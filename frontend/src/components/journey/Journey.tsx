09d3cf4fc6a8166ba0e43bdfe802de90218fe7b7
Nil ANDREU <nil.andreu@veepee.com>
2022-07-02 11:28:09 +0200

    Scrolling interactivity

---
+++ frontend/src/components/journey/Journey.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Journey() {
+  return (
+    <Container id="journey">Journey</Container>
+  )
+}
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+`
+
+export default Journey
