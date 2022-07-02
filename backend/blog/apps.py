4911d43943cd5e26df275e6c8d6065419762b5ef
Nil ANDREU <nil.andreu@veepee.com>
2022-07-02 09:19:48 +0200

    Type hinting

--- backend/blog/apps.py
+++ backend/blog/apps.py
@@ -1,6 +1,7 @@
+import string
 from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog'
+    default_auto_field: string = 'django.db.models.BigAutoField'
+    name: string = 'blog'
